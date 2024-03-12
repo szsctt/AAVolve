ab254e44632b3d815c1bdee2e383e0512f2b8aeb
Suzanne Scott <sscott@cmri.org.au>
2024-03-12 15:59:53 +1100

    added script to sum counts for the same sequences from stdin and print to stdout, and tests

---
+++ tests/pytest/test_sum_counts.py
@@ -0,0 +1,60 @@
+from io import StringIO
+import pytest
+
+from scripts.sum_counts import main
+
+class TestMain:
+
+    def test_main(self, monkeypatch, capsys):
+    
+        # set sys.stdin
+        monkeypatch.setattr('sys.stdin', StringIO("1\tA\n1\tA\n1\tA\n1\tB\n1\tB\n1\tC\n"))
+
+        # run main
+        main()
+
+        # check output
+        captured = capsys.readouterr()
+        assert captured.out == "3\tA\n2\tB\n1\tC\n"
+
+    def test_main_empty(self, monkeypatch, capsys):
+
+        # set sys.stdin
+        monkeypatch.setattr('sys.stdin', StringIO(""))
+
+        # run main
+        main()
+
+        # check output
+        captured = capsys.readouterr()
+        assert captured.out == ""
+
+    def test_main_single(self, monkeypatch, capsys):
+
+        # set sys.stdin
+        monkeypatch.setattr('sys.stdin', StringIO("1\tA\n"))
+
+        # run main
+        main()
+
+        # check output
+        captured = capsys.readouterr()
+        assert captured.out == "1\tA\n"
+
+    def test_main_with_header(self, monkeypatch):
+
+        # set sys.stdin
+        monkeypatch.setattr('sys.stdin', StringIO("count\tsequence\n1\tA\n1\tA\n1\tA\n1\tB\n1\tB\n1\tC\n"))
+
+        # run main
+        with pytest.raises(ValueError):
+            main()
+
+    def test_main_unsorted(self, monkeypatch):
+
+        # set sys.stdin
+        monkeypatch.setattr('sys.stdin', StringIO("1\tA\n1\tB\n2\tA\n1\tB\n1\tB\n1\tC\n"))
+
+        # run main
+        with pytest.raises(ValueError):
+            main()
