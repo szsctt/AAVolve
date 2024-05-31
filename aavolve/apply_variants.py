6e8ee39463224d065d0fe889bac3c5952072e55e
Suzanne Scott <sscott@cmri.org.au>
2024-05-31 18:40:20 +1000

    Updated apply_variants.py to group variants if specified when creating sequence file from parent file.  If there are multiple parents, pick one at random.

--- aavolve/apply_variants.py
+++ aavolve/apply_variants.py
@@ -11,7 +11,7 @@ import tempfile
 from Bio import Seq
 from Bio import SeqIO
 
-from aavolve.utils import use_open, sort_var_names, get_parents, get_reference_name
+from aavolve.utils import use_open, sort_var_names, get_parents, get_reference_name, make_var_groups
 
 def main():
     
@@ -21,6 +21,8 @@ def main():
     parser.add_argument('-r', '--reference', help='Reference sequence (fasta)', required=True)
     parser.add_argument('-t', '--translate', help='Translate to amino acids', action='store_true')
     parser.add_argument('-f', '--fasta', help='Output in fasta format instead of tab-delimited', action='store_true')
+    parser.add_argument("--group-vars", '-g', action="store_true", help="Group adjacent variants and use the parent with the lowest hamming distance")
+    parser.add_argument("--group-dist", help="Variants separated by at most this number of nucleotides will be grouped for assigning parents", default=1, type=int)
     parser.add_argument('-o', '--output', help='Output file', required=True)
     args = parser.parse_args()
 
@@ -29,7 +31,7 @@ def main():
 
     # if parents are supplied, read in and use to create temp file that contains sequences for each variant
     if args.parents:
-        f = create_temp_seq_file(args.variants, args.parents)
+        f = create_temp_seq_file(args.variants, args.parents, args.group_vars, args.group_dist)
         variants_file = f.name
     else:
         variants_file = args.variants
@@ -169,13 +171,16 @@ def read_reference(reference):
     assert len(recs) == 1
     return str(recs[0].seq)
 
-def create_temp_seq_file(variants, parents):
+def create_temp_seq_file(variants, parents, do_grouping, grouping_dist):
     """
     Create a temporary file with sequences for each variant
     """
 
     # read in parents
     vars = get_parents(parents)
+
+    # group variants
+    groups = make_var_groups(sort_var_names(vars.keys()), do_grouping, grouping_dist)
 
     f = tempfile.NamedTemporaryFile(mode='w+t')
     with use_open(variants, 'rt') as p:
@@ -184,8 +189,14 @@ def create_temp_seq_file(variants, parents):
         # write header to output
         f.write('\t'.join(reader.fieldnames) + '\n')
 
+        # check that variants are the same as parent file
+        file_vars = set(reader.fieldnames)
+        file_vars.discard('count')
+        file_vars.discard('read_id')
+        assert set(vars.keys()) == set(file_vars)
+
         # iterate over rows
-        for row in reader:
+        for rnum, row in enumerate(reader):
             
             # get count and read_id
             if 'count' in row:
@@ -197,31 +208,31 @@ def create_temp_seq_file(variants, parents):
             
             # iterate over parents
             seqs = []
-            for pvar in row:
+            for g in groups:
 
-                if pvar not in vars:
-                    raise ValueError(f"Variant {pvar} not found in parent variants file")
+                # get all possible parents for this group
+                pars = [row[i] for i in g]
                 
-                # get possible parents for this variant
-                parents_row = row[pvar].split(',')
-                pos_vars = vars[pvar] # get the variant objects for this position
+                # all parents should be the same within the group
+                assert all([i == pars[0] for i in pars])
+                
+                # if there are multiple possible parents, pick one at random
+                g_parent = set(pars[0].split(',')).pop()
 
-                # get the sequences of each parent allele
-                par_seqs = []
-                for i in parents_row:
-                    # get alt allele if this is in the list of parent variants
-                    if i in pos_vars:
-                        par_seqs.append(pos_vars[i].qbases())
-                    # otherwise, assume it's the reference allele
+                for pvar in g:
+                    
+                    # get possible parents for this variant
+                    pos_vars = vars[pvar] # get the variant objects for this position
+
+                    # get the sequences of each parent allele
+                    if g_parent in pos_vars.keys():
+                        # get alt allele if this is in the list of parent variants
+                        seqs.append(pos_vars[g_parent].qbases())
                     else:
-                        var = list(pos_vars.values())[0]
-                        par_seqs.append(var.refbases())
+                        # otherwise, assume it's the reference allele
+                        seqs.append(pos_vars[list(pos_vars.keys())[0]].refbases())
 
-                # check to make sure we got at least one sequence
-                assert len(par_seqs) > 0
-                # all bases should be the same
-                assert all([i==par_seqs[0] for i in par_seqs]) 
-                seqs.append(par_seqs[0])
+            assert len(seqs) == len(row.keys())
 
             # write row to file
             if 'read_id' in reader.fieldnames:
